325cd5b99c31bcb8c25e1d49a28e797670159cee
Sasha <alexandra.aslobodian@gmail.com>
2023-05-14 10:23:45 -0400

    added PasswordEncoder Bean

--- src/main/java/com/learn/TicketingSecurityApplication.java
+++ src/main/java/com/learn/TicketingSecurityApplication.java
@@ -3,6 +3,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication  //this includes @Configuration
 public class TicketingSecurityApplication {
@@ -15,6 +17,11 @@ public class TicketingSecurityApplication {
     @Bean
     public ModelMapper mapper(){
         return new ModelMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
